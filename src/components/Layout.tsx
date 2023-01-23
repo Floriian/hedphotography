9036bde0285e5e4274389ee51f0ab0591136f8b0
Floriian <lovasifloriian@outlook.hu>
2023-01-24 00:00:38 +0100

    create layout, because menu doesn't working.  Menu rendered on server

---
+++ src/components/Layout.tsx
@@ -0,0 +1,16 @@
+import Menu from "./Menu";
+
+type Props = {
+  children: React.ReactNode;
+};
+
+export default function Layout({ children }: Props) {
+  return (
+    <>
+      <div className="fixed w-full">
+        <Menu />
+      </div>
+      <div className="relative -z-10">{children}</div>
+    </>
+  );
+}
