bbece1c52ac2582ab40dfcd6077915c773c53e2e
Floriian <lovasifloriian@outlook.hu>
2023-01-23 18:27:38 +0100

    TODO:  menu

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,9 +1,20 @@
 import Image from "next/image";
 import Link from "next/link";
+import { useRouter } from "next/router";
+import { useScroll } from "../hooks";
+import React, { useEffect, useState } from "react";
+import { GetServerSideProps } from "next";
 
 function Menu() {
+  const scroll = useScroll();
+  useEffect(() => console.log(scroll), [scroll]);
+
   return (
-    <nav className="w-full flex">
+    <nav
+      className={
+        scroll <= 200 ? "w-full flex bg-blue-500" : "w-full flex bg-red-500"
+      }
+    >
       <Image
         alt="Logo"
         src="/favicon.ico"
@@ -14,25 +25,25 @@ function Menu() {
       <div className="flex w-full justify-center m-5">
         <Link
           href="/"
-          className="p-2 text-black hover:text-gray-600 hover:underline-offset-2 hover:underline transition-colors duration-150"
+          className="p-2  hover:text-gray-600 hover:underline-offset-2 hover:underline transition-colors duration-150"
         >
           Főoldal
         </Link>
         <Link
           href="/about-me"
-          className="p-2 text-black hover:text-gray-600 hover:underline-offset-2 hover:underline transition-colors duration-150"
+          className="p-2  hover:text-gray-600 hover:underline-offset-2 hover:underline transition-colors duration-150"
         >
           Rólam
         </Link>
         <Link
           href="/galery"
-          className="p-2 text-black hover:text-gray-600 hover:underline-offset-2 hover:underline transition-colors duration-150"
+          className="p-2  hover:text-gray-600 hover:underline-offset-2 hover:underline transition-colors duration-150"
         >
           Galéria
         </Link>
         <Link
           href="/contact"
-          className="p-2 text-black hover:text-gray-600 hover:underline-offset-2 hover:underline transition-colors duration-150"
+          className="p-2  hover:text-gray-600 hover:underline-offset-2 hover:underline transition-colors duration-150"
         >
           Kapcsolat
         </Link>
