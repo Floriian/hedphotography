b7800cc4566853ed9230248c7dae91c2769e9d22
Floriian <lovasifloriian@outlook.hu>
2023-01-23 18:26:39 +0100

    Add fontLoader.

--- next.config.js
+++ next.config.js
@@ -1,6 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  images: {
+    domains: ["floxbaayonmzzoqtasrc.supabase.co"],
+  },
+  experimental: {
+    fontLoaders: [
+      {
+        loader: "@next/font/google",
+        options: {
+          subsets: ["latin"],
+        },
+      },
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
