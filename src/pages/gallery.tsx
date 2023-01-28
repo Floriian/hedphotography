a4aea14cda2da11028fe62ffa82d01c5eb4e56a7
Floriian <lovasifloriian@outlook.hu>
2023-01-28 14:52:35 +0100

    setting up for adminui.

---
+++ src/pages/gallery.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Layout from '../components/Layout';
+
+//TODO
+
+function gallery() {
+  return <div>gallery</div>;
+}
+
+gallery.Layout = Layout;
+
+export default gallery;
