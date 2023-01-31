83dac19929c8e1481de472d59580233e1642bf3b
Floriian <lovasifloriian@outlook.hu>
2023-01-31 20:53:21 +0100

    fill with lorem ipsum

--- src/pages/about-me.tsx
+++ src/pages/about-me.tsx
@@ -2,15 +2,37 @@ import React from 'react';
 
 type Props = {};
 
+//TODO
+
 function AboutMe({}: Props) {
   return (
-    <div className="flex h-screen justify-center">
-      <div className="m-auto max-w-2xl">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi rerum,
-        ullam dolore accusantium maxime nesciunt, magnam itaque consequatur vero
-        corporis quidem voluptatum esse qui error recusandae quas! Delectus,
-        praesentium neque!
-      </div>
+    <div>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi rerum,
+      ullam dolore accusantium maxime nesciunt, magnam itaque consequatur vero
+      corporis quidem voluptatum esse qui error recusandae quas! Delectus,
+      praesentium neque!Lorem ipsum dolor sit amet consectetur adipisicing elit.
+      Animi rerum, ullam dolore accusantium maxime nesciunt, magnam itaque
+      consequatur vero corporis quidem voluptatum esse qui error recusandae
+      quas! Delectus, praesentium neque!Lorem ipsum dolor sit amet consectetur
+      adipisicing elit. Animi rerum, ullam dolore accusantium maxime nesciunt,
+      magnam itaque consequatur vero corporis quidem voluptatum esse qui error
+      recusandae quas! Delectus, praesentium neque!Lorem ipsum dolor sit amet
+      consectetur adipisicing elit. Animi rerum, ullam dolore accusantium maxime
+      nesciunt, magnam itaque consequatur vero corporis quidem voluptatum esse
+      qui error recusandae quas! Delectus, praesentium neque!Lorem ipsum dolor
+      sit amet consectetur adipisicing elit. Animi rerum, ullam dolore
+      accusantium maxime nesciunt, magnam itaque consequatur vero corporis
+      quidem voluptatum esse qui error recusandae quas! Delectus, praesentium
+      neque!Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi
+      rerum, ullam dolore accusantium maxime nesciunt, magnam itaque consequatur
+      vero corporis quidem voluptatum esse qui error recusandae quas! Delectus,
+      praesentium neque!Lorem ipsum dolor sit amet consectetur adipisicing elit.
+      Animi rerum, ullam dolore accusantium maxime nesciunt, magnam itaque
+      consequatur vero corporis quidem voluptatum esse qui error recusandae
+      quas! Delectus, praesentium neque!Lorem ipsum dolor sit amet consectetur
+      adipisicing elit. Animi rerum, ullam dolore accusantium maxime nesciunt,
+      magnam itaque consequatur vero corporis quidem voluptatum esse qui error
+      recusandae quas! Delectus, praesentium neque!
     </div>
   );
 }
