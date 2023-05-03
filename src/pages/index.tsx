903a5fe3be15a4d712b5569934dd5eff0fb73ad0
Floriian <lovasifloriian@outlook.hu>
2023-05-03 15:14:44 +0200

    index page now use api

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,4 @@
 import React, { useEffect } from 'react';
-import { supabase } from '../utils/supabase';
 import Image from '../components/Image';
 import SideCard from '../components/SideCard';
 import HeroImage from '../components/HeroImage';
@@ -10,52 +9,24 @@ const allison = Allison({
 });
 
 type Props = {
-  images: Array<{ publicUrl: string }>;
-  aboutme: Array<{ text: string }>;
-  heroImage: {
-    publicUrl: string;
-  };
+  images: Array<{ name: string; publicUrl: string }>;
 };
 
 export async function getStaticProps() {
-  let images: Array<{ publicUrl: string }> = [];
-  const { data: imageNames } = await supabase.storage
-    .from('images')
-    .list('gallery');
-
-  //Todo: If no images, return placeholders.
-
-  for (let img of imageNames!) {
-    const { data: imageData } = await supabase.storage
-      .from('images')
-      .getPublicUrl(`gallery/${img.name}`);
-
-    images.push(imageData);
-  }
-
-  const { data: heroImage } = await supabase.storage
-    .from('images')
-    .getPublicUrl('mainpage/home.jpg');
-
-  const { data: aboutme } = await supabase.from('about_me').select('text');
+  const response = await fetch('http://Localhost:3000/api/getGalleryImages');
+  const images = await response.json();
   return {
     props: {
       images,
-      aboutme,
-      heroImage,
     },
   };
 }
 
-function Index({ images, aboutme, heroImage }: Props) {
-  const randomImages = images
-    .sort(() => Math.random() - Math.random())
-    .slice(0, 9);
-
+function Index({ images }: Props) {
   return (
     <div className="relative -z-10">
       <section>
-        <HeroImage url={heroImage.publicUrl}>
+        {/* <HeroImage url={heroImage.publicUrl}>
           <h1 className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-center text-xl uppercase text-white">
             The art of{' '}
             <span className={allison.className + ' text-5xl'}>
@@ -63,26 +34,20 @@ function Index({ images, aboutme, heroImage }: Props) {
               Photography
             </span>
           </h1>
-        </HeroImage>
+        </HeroImage> */}
       </section>
       <section className="flex justify-center">
-        <SideCard
+        {/* <SideCard
           content={aboutme[0].text}
           href="/about-me"
           image="/favicon.ico"
           imageAlt="Me "
-        />
+        /> */}
       </section>
       <section className="container mx-auto p-10">
         <div className="grid gap-3 sm:grid-cols-1 md:grid-cols-3">
-          {randomImages.map((img) => (
-            <Image
-              alt="Image"
-              height={600}
-              url={img.publicUrl}
-              width={600}
-              key={img.publicUrl}
-            />
+          {images.map((img) => (
+            <p key={img.publicUrl}>{img.publicUrl}</p>
           ))}
         </div>
       </section>
