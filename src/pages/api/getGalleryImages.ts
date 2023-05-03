8a322feaa211095a1742d742f6d9054b665d2a7e
Floriian <lovasifloriian@outlook.hu>
2023-05-03 15:14:30 +0200

    read files from "./gallery/

---
+++ src/pages/api/getGalleryImages.ts
@@ -0,0 +1,17 @@
+import { readdirSync } from 'fs';
+import { NextRequest, NextResponse } from 'next/server';
+import path from 'path';
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default async (req: NextRequest, res: NextResponse) => {
+  const dir = path.resolve('./public/gallery');
+
+  const files = await readdirSync(dir);
+  const images = files.map((name) => {
+    return {
+      name,
+      publicUrl: path.join('/', 'gallery', name),
+    };
+  });
+  res.json(images);
+};
