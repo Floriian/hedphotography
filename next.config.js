daf2f0e34983a382e01e41990e20b1d557eb2e34
Floriian <lovasifloriian@outlook.hu>
2023-05-07 16:46:56 +0200

    format

--- next.config.js
+++ next.config.js
@@ -2,14 +2,14 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ["floxbaayonmzzoqtasrc.supabase.co"],
+    domains: ['floxbaayonmzzoqtasrc.supabase.co'],
   },
   experimental: {
     fontLoaders: [
       {
-        loader: "@next/font/google",
+        loader: '@next/font/google',
         options: {
-          subsets: ["latin"],
+          subsets: ['latin'],
         },
       },
     ],
